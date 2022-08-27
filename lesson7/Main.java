8654a5e6cfc1800937c5415970c540de1853b1af
olgaseitz <79039523+olgaseitz@users.noreply.github.com>
2022-08-27 21:33:11 +0300

    Delete Main.java

--- lesson7/Main.java
+++
@@ -1,8 +0,0 @@
-package lesson7;
-
-public class Main {
-    public static void main(String[] args) {
-        UserInterfaceView userInterfaceView = new UserInterfaceView();
-        userInterfaceView.runInterface();
-    }
-}
